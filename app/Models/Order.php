8b5494197a5995549f62a7f5c23ebd6df69b3fdc
SergioGomezSantos <gomez.sergio.smr@gmail.com>
2023-02-22 18:17:54 +0100

    Fechas

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Cliente;
+use Carbon\Carbon;
 
 class Order extends Model
 {
@@ -15,9 +17,20 @@ class Order extends Model
         return $this->belongsToMany(Cliente::class);
     }
 
-    protected $dates = ['fecha', ];
-
+    // protected $dates = ['fecha', ];
     // $user->fecha->format('d-m-Y')
     // $user->fecha->addDays(30)
     // $fecha->diffInDays($dt->copy()->addMonth())
+
+    protected $casts = [
+        "fecha" => "datetime:d-m-Y"
+    ];
+
+    protected function Fecha(): Attribute
+    {
+        return new Attribute(
+            fn ($value) => Carbon::parse($value)->format('d-m-Y'),
+            fn ($value) => Carbon::parse($value)->format('d/m/Y'),
+        );
+    }
 }
